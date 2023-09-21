3297cb925369330f2c89d04088c3f5e9be240deb
kvtsuki <117033172+kvtsuki@users.noreply.github.com>
2023-09-21 19:58:04 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите координату x1: ");
+                double x1 = double.Parse(Console.ReadLine());
+
+                Console.Write("Введите координату y1: ");
+                double y1 = double.Parse(Console.ReadLine());
+
+                Console.Write("Введите координату x2: ");
+                double x2 = double.Parse(Console.ReadLine());
+
+                Console.Write("Введите координату y2: ");
+                double y2 = double.Parse(Console.ReadLine());
+
+                double k = (y1 - y2) / (x1 - x2);
+
+                double b = y2 - k * x2;
+
+                Console.WriteLine($"Уравнение прямой: y = {k}x + {b}");
+            }
+            catch
+            {
+                Console.WriteLine("Введен неверный  тип данных");
+            }
+        }
+    }
+}
